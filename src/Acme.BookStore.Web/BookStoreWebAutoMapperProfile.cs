a9950067fd10ee04b6b80c65735c79476c41308b
OmranAlaaEldein <omranalaaeldein@gmail.com>
2022-03-20 10:22:50 +0200

    create books store project

---
+++ src/Acme.BookStore.Web/BookStoreWebAutoMapperProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Acme.BookStore.Books;
+
+namespace Acme.BookStore.Web
+{
+    public class BookStoreWebAutoMapperProfile : Profile
+    {
+        public BookStoreWebAutoMapperProfile()
+        {
+            CreateMap<BookDto, CreateUpdateBookDto>();
+        }
+    }
+}
